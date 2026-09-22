13d0903e2e48d4ec600374e9078f53133ce7bc58
Carlos O'Ryan <coryan@google.com>
2026-09-21 20:42:28 -0400

    chore: update librarian w/ better enum docs (#1017)
    In this version of librarian we improve the generated documentation for
    `enum` so the possible evolution and usage of the enums is more obvious.
    For example, we recommend using `@unknown default:` so they get warnings
    when new values appear in the enum.

--- Sources/GoogleIAMV1/AuditLogConfig.swift
+++ Sources/GoogleIAMV1/AuditLogConfig.swift
@@ -108,6 +108,12 @@ public struct AuditLogConfig: Codable, Equatable, GoogleWKT._AnyPackable,
 
   /// The list of valid permission types for which logging can be configured.
   /// Admin writes are always logged, and are not configurable.
+  ///
+  /// - Note: Adding cases to this enumeration is not considered a breaking change.
+  ///   Always include an `@unknown default:` case when switching over this type.
+  ///   Do not pattern-match against `unknownStringValue` or `unknownIntValue`
+  ///   expecting specific values to remain unparsed; future releases may promote
+  ///   them to named cases.
   public enum LogType: Codable, Equatable, Sendable {
     /// Default case. Should never be this.
     case unspecified
@@ -119,15 +125,21 @@ public struct AuditLogConfig: Codable, Equatable, GoogleWKT._AnyPackable,
     case dataRead
     /// Encodes an unknown integer value.
     ///
-    /// The most common cause for an unknown values is for the service to send
+    /// The most common cause for an unknown value is for the service to send
     /// a value unknown to the library. We recommend you update your library to
     /// the latest version.
+    ///
+    /// - Warning: Do not pattern-match specific integer values in this case;
+    ///   future releases may promote them to named enum cases.
     case unknownIntValue(Int)
     /// Encodes an unknown string value.
     ///
-    /// The most common cause for an unknown values is for the service to send
+    /// The most common cause for an unknown value is for the service to send
     /// a value unknown to the library. We recommend you update your library to
     /// the latest version.
+    ///
+    /// - Warning: Do not pattern-match specific string literals in this case;
+    ///   future releases may promote them to named enum cases.
     case unknownStringValue(String)
 
     public init() {
